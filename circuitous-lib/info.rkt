6a15fa97c8befd38e6fb709126baa0095cbf3b1c
Robby Findler <robby@racket-lang.org>
2022-06-27 19:08:33 -0500

    depend on last commit of rosette v3.2

--- circuitous-lib/info.rkt
+++ circuitous-lib/info.rkt
@@ -5,7 +5,7 @@
 (define deps
   '("redex-lib"
     ("base" #:version "7.4")
-    "rosette"))
+    "https://github.com/emina/rosette.git#c092b65b8831726487edd1e63b4390508db483d2"))
 
 
 (define build-deps '("rackunit-lib"
--- circuitous-test/info.rkt
+++ circuitous-test/info.rkt
@@ -4,7 +4,7 @@
 
 (define deps
   '(("base" #:version "7.4")
-    "rosette"
+    "https://github.com/emina/rosette.git#c092b65b8831726487edd1e63b4390508db483d2"
     "circuitous-lib"))
 
 
